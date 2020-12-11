1632ccf9bb5623de2717ac878d20b8a15bbe6028
anshoryihsan <anshoryihsan@.gmail.com>
2020-12-11 09:50:11 +0700

    update

--- src/Helper/axios.js
+++ src/Helper/axios.js
@@ -2,7 +2,7 @@ import Axios from "axios";
 
 export default Axios.create({
   // baseURL: "wasdqe.herokuapp.com/api/v1",
-  baseURL: "http://54.145.220.98:8000/api/v1",
+  baseURL: "http://52.207.113.163/api/v1",
   // baseURL: "http://127.0.0.1:8000/api/v1",
 });
 
