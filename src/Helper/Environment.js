761ec295b2bd81c2b930f63170c5d8954fc8cfca
anshoryihsan <anshoryihsan@.gmail.com>
2020-12-12 07:37:53 +0700

    deploy

--- src/Helper/Environment.js
+++ src/Helper/Environment.js
@@ -1,4 +1,4 @@
-// export const IMAGE_URL = "http://52.207.113.163:8000/image/";
+export const IMAGE_URL = "http://52.207.113.163:8000/image/";
 // export const SOCKET_URL = "http://52.207.113.163:8000/";
-export const IMAGE_URL = "http://127.0.0.1:8000/image/";
+// export const IMAGE_URL = "http://127.0.0.1:8000/image/";
 // export const SOCKET_URL = "http://127.0.0.1:8000/";
--- src/Helper/axios.js
+++ src/Helper/axios.js
@@ -2,6 +2,6 @@ import Axios from "axios";
 
 export default Axios.create({
   // baseURL: "wasdqe.herokuapp.com/api/v1",
-  // baseURL: "http://52.207.113.163/api/v1",
-  baseURL: "http://127.0.0.1:8000/api/v1",
+  baseURL: "http://52.207.113.163:8000/api/v1",
+  // baseURL: "http://127.0.0.1:8000/api/v1",
 });
